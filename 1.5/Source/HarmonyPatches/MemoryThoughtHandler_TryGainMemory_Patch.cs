be26482cd9d73dd4c232ad5bb478ddc5bed8934e
Taranchuk <nfhfyxer@gmail.com>
2024-04-26 21:18:12 +0300

    fix insult/slight thoughts opinion offsets not being inversed for manipulative

---
+++ 1.5/Source/HarmonyPatches/MemoryThoughtHandler_TryGainMemory_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[] { typeof(Thought_Memory), typeof(Pawn) })]
+    public static class MemoryThoughtHandler_TryGainMemory_Patch
+    {
+        public static void Postfix(Thought_Memory newThought, Pawn otherPawn)
+        {
+            if (newThought is Thought_MemorySocial socialMemory && otherPawn.HasTrait(ST_DefOf.ST_Manipulative))
+            {
+                if (newThought.def == ST_DefOf.Insulted || newThought.def == ST_DefOf.Slighted)
+                {
+                    socialMemory.opinionOffset = -socialMemory.opinionOffset;
+                }
+            }
+        }
+    }
+}
