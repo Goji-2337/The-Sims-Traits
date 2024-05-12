13a87faee31d7aeba741d424550a37c4bcdc8d11
Taranchuk <nfhfyxer@gmail.com>
2024-05-12 16:21:33 +0300

    paranoid

--- 1.5/Source/ST_DefOf.cs
+++ 1.5/Source/ST_DefOf.cs
@@ -1,15 +1,16 @@
 ﻿using RimWorld;
+using Verse;
 
 namespace SimsTraits
 {
     [DefOf]
     public static class ST_DefOf
     {
-        public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak, ST_Emotional;
-        public static ThoughtDef Insulted, Slighted;
+        public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak, ST_Emotional,
+            ST_Paranoid;
+        public static ThoughtDef Insulted, Slighted, ST_ParanoidThought;
         public static InteractionDef Slight;
-
+        public static SoundDef LetterArrive_BadUrgentBig, LetterArrive_BadUrgent, LetterArrive_BadUrgentSmall, LetterArrive_Good;
+        public static HediffDef ST_AdrenalineRush;
     }
-
-
 }
