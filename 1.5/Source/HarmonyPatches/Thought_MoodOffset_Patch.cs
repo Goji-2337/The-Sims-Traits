a625582e6c2704a934c364c183940c9b5c979294
Taranchuk <nfhfyxer@gmail.com>
2024-05-12 15:29:21 +0300

    emotional

---
+++ 1.5/Source/HarmonyPatches/Thought_MoodOffset_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(Thought), "MoodOffset")]
+    public static class Thought_MoodOffset_Patch
+    {
+        public static void Postfix(ref float __result, Thought __instance)
+        {
+            if (__instance.pawn.HasTrait(ST_DefOf.ST_Emotional))
+            {
+                __result *= 1.15f;
+            }
+        }
+    }
+}
--- 1.5/Source/ST_DefOf.cs
+++ 1.5/Source/ST_DefOf.cs
@@ -5,9 +5,11 @@ namespace SimsTraits
     [DefOf]
     public static class ST_DefOf
     {
-        public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak;
+        public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak, ST_Emotional;
         public static ThoughtDef Insulted, Slighted;
         public static InteractionDef Slight;
 
     }
+
+
 }
