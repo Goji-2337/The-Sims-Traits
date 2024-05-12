b0dc1e97faa82174e9a8a20b7901d296dedb0622
Taranchuk <nfhfyxer@gmail.com>
2024-05-12 14:58:35 +0300

    health freak

---
+++ 1.5/Source/HarmonyPatches/AgeInjuryUtility_GenerateRandomOldAgeInjuries_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(AgeInjuryUtility), "GenerateRandomOldAgeInjuries")]
+    public static class AgeInjuryUtility_GenerateRandomOldAgeInjuries_Patch
+    {
+        public static bool Prefix(Pawn pawn)
+        {
+            if (pawn.HasTrait(ST_DefOf.ST_HealthFreak))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- 1.5/Source/ST_DefOf.cs
+++ 1.5/Source/ST_DefOf.cs
@@ -5,8 +5,9 @@ namespace SimsTraits
     [DefOf]
     public static class ST_DefOf
     {
-        public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist;
+        public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak;
         public static ThoughtDef Insulted, Slighted;
         public static InteractionDef Slight;
+
     }
 }
