4da9c5b5481ff71ea6e881184ebf5e2f8e016304
Taranchuk <nfhfyxer@gmail.com>
2024-05-12 16:54:06 +0300

    goofball

---
+++ 1.5/Source/MentalState_GoofballGiggling.cs
@@ -0,0 +1,71 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace SimsTraits
+{
+    public class MentalState_GoofballGiggling : MentalState
+    {
+        public const float BabyScreamRadius = 9.9f;
+
+        public const int ScreamInterval = 150;
+
+        private float lastScreamTick = -1f;
+
+        private List<Pawn> alreadyHeard = new List<Pawn>(32);
+
+        public override RandomSocialMode SocialModeMax()
+        {
+            return RandomSocialMode.Normal;
+        }
+
+        public override void MentalStateTick()
+        {
+            base.MentalStateTick();
+            if ((float)Find.TickManager.TicksGame <= lastScreamTick + 150f || pawn.IsWorldPawn())
+            {
+                return;
+            }
+            Caravan caravan;
+            if ((caravan = pawn.GetCaravan()) != null)
+            {
+                foreach (Pawn item in caravan.PawnsListForReading)
+                {
+                    DoPawnHear(pawn, item);
+                }
+            }
+            else
+            {
+                GenClamor.DoClamor(pawn, 9.9f, DoPawnHear);
+            }
+            lastScreamTick = Find.TickManager.TicksGame;
+        }
+
+        private void DoPawnHear(Thing source, Pawn hearer)
+        {
+            if (hearer != source && !alreadyHeard.Contains(hearer))
+            {
+                alreadyHeard.Add(hearer);
+                AuraEffect(source, hearer);
+            }
+        }
+
+        private void AuraEffect(Thing source, Pawn hearer)
+        {
+            if (source is Pawn otherPawn && hearer.needs.mood != null)
+            {
+                hearer.needs.mood.thoughts.memories.TryGainMemory(ST_DefOf.ST_GigglingGoofball, otherPawn);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref lastScreamTick, "lastScreamTick", 0f);
+            Scribe_Collections.Look(ref alreadyHeard, "alreadyHeard", LookMode.Reference);
+        }
+    }
+}
--- 1.5/Source/ST_DefOf.cs
+++ 1.5/Source/ST_DefOf.cs
@@ -7,10 +7,11 @@ namespace SimsTraits
     public static class ST_DefOf
     {
         public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak, ST_Emotional,
-            ST_Paranoid;
-        public static ThoughtDef Insulted, Slighted, ST_ParanoidThought;
+            ST_Paranoid, ST_Goofball;
+        public static ThoughtDef Insulted, Slighted, ST_ParanoidThought, ST_GigglingGoofball;
         public static InteractionDef Slight;
         public static SoundDef LetterArrive_BadUrgentBig, LetterArrive_BadUrgent, LetterArrive_BadUrgentSmall, LetterArrive_Good;
         public static HediffDef ST_AdrenalineRush;
+        public static MentalFitDef ST_GoofballGiggling;
     }
 }
