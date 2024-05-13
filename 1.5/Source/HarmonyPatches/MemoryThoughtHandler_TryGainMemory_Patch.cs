17856ded79739edc149f3555aef85051155f5511
Taranchuk <nfhfyxer@gmail.com>
2024-05-13 21:55:02 +0300

    grumpy

--- 1.5/Source/HarmonyPatches/MemoryThoughtHandler_TryGainMemory_Patch.cs
+++ 1.5/Source/HarmonyPatches/MemoryThoughtHandler_TryGainMemory_Patch.cs
@@ -8,13 +8,30 @@ namespace SimsTraits
     [HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[] { typeof(Thought_Memory), typeof(Pawn) })]
     public static class MemoryThoughtHandler_TryGainMemory_Patch
     {
-        public static void Postfix(Thought_Memory newThought, Pawn otherPawn)
+        public static void Postfix(MemoryThoughtHandler __instance, Thought_Memory newThought, Pawn otherPawn)
         {
             if (newThought is Thought_MemorySocial socialMemory && otherPawn.HasTrait(ST_DefOf.ST_Manipulative))
             {
                 if (newThought.def == ST_DefOf.Insulted || newThought.def == ST_DefOf.Slighted)
                 {
                     socialMemory.opinionOffset = -socialMemory.opinionOffset;
+                }
+            }
+
+            if (newThought.pawn.HasTrait(ST_DefOf.ST_Grumpy) && newThought.MoodOffset() > 0)
+            {
+                var tile = __instance.pawn.Tile;
+                if (tile != -1)
+                {
+                    var day = GenLocalDate.DayOfQuadrum(tile);
+                    if (day == 0 || Rand.Chance(0.15f))
+                    {
+                        newThought.moodPowerFactor = 0;
+                    }
+                }
+                else if (Rand.Chance(0.15f))
+                {
+                    newThought.moodPowerFactor = 0;
                 }
             }
         }
--- 1.5/Source/ST_DefOf.cs
+++ 1.5/Source/ST_DefOf.cs
@@ -7,7 +7,7 @@ namespace SimsTraits
     public static class ST_DefOf
     {
         public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak, ST_Emotional,
-            ST_Paranoid, ST_Loyal, ST_PartyAnimal, ST_Insane, ST_Proper;
+            ST_Paranoid, ST_Loyal, ST_PartyAnimal, ST_Insane, ST_Proper, ST_Grumpy;
         [MayRequireBiotech] public static TraitDef ST_Goofball;
         [MayRequireBiotech] public static ThoughtDef ST_GigglingGoofball;
         public static ThoughtDef Insulted, Slighted, ST_ParanoidThought, ST_LoyalThought;
