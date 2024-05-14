3b5fa0960c06303131b07b79cd5b37079d087169
Taranchuk <nfhfyxer@gmail.com>
2024-05-14 22:54:56 +0300

    vegan

--- 1.5/Source/ST_DefOf.cs
+++ 1.5/Source/ST_DefOf.cs
@@ -7,7 +7,8 @@ namespace SimsTraits
     public static class ST_DefOf
     {
         public static TraitDef ST_Manipulative, ST_Naive, ST_Narcissist, ST_HealthFreak, ST_Emotional,
-            ST_Paranoid, ST_Loyal, ST_PartyAnimal, ST_Insane, ST_Proper, ST_Grumpy, ST_Shy, ST_Materialistic;
+            ST_Paranoid, ST_Loyal, ST_PartyAnimal, ST_Insane, ST_Proper, ST_Grumpy, ST_Shy, ST_Materialistic,
+            ST_Vegan;
         [MayRequireBiotech] public static TraitDef ST_Goofball;
         [MayRequireBiotech] public static ThoughtDef ST_GigglingGoofball;
         public static ThoughtDef Insulted, Slighted, ST_ParanoidThought, ST_LoyalThought, KindWordsMood, ST_NewStuff;
