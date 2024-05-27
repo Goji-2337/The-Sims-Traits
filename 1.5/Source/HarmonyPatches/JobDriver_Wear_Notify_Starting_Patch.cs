19f05b6ee3cbe6e3cc49b02f2bdb9b7bee8af76b
Taranchuk <nfhfyxer@gmail.com>
2024-05-27 13:25:16 +0300

    tech whiz

---
+++ 1.5/Source/HarmonyPatches/JobDriver_Wear_Notify_Starting_Patch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(JobDriver_Wear), "Notify_Starting")]
+    public static class JobDriver_Wear_Notify_Starting_Patch
+    {
+        public static void Postfix(JobDriver_Wear __instance)
+        {
+            __instance.duration += DelayDuration(__instance, __instance.Apparel);
+        }
+
+        public static int DelayDuration(JobDriver_Wear job, Thing thing)
+        {
+            var comp = thing.TryGetComp<CompBiocodable>();
+            if (comp != null && comp.CodedPawn != job.pawn)
+            {
+                return 17 * 60;
+            }
+            return 0;
+        }
+    }
+}
