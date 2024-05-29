721747a5c026c7905cd6f42d18ec2ab855dcc09b
Taranchuk <nfhfyxer@gmail.com>
2024-05-29 13:54:16 +0300

    submissive

---
+++ 1.5/Source/HarmonyPatches/SlaveRebellionUtility_CanParticipateInSlaveRebellion_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(SlaveRebellionUtility), "CanParticipateInSlaveRebellion")]
+    public static class SlaveRebellionUtility_CanParticipateInSlaveRebellion_Patch
+    {
+        public static void Postfix(ref bool __result, Pawn pawn)
+        {
+            if (pawn.HasTrait(ST_DefOf.ST_Submissive))
+            {
+                __result = false;
+            }
+        }
+    }
+}
--- 1.5/Source/TraitUtils.cs
+++ 1.5/Source/TraitUtils.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System.Collections.Generic;
 using Verse;
 
 namespace SimsTraits
@@ -8,11 +9,28 @@ namespace SimsTraits
     public static class TraitUtils
     {
         public static bool VEFLoaded = ModsConfig.IsActive("OskarPotocki.VanillaFactionsExpanded.Core");
+        public static bool VETraitsLoaded = ModsConfig.IsActive("VanillaExpanded.VanillaTraitsExpanded");
+        public static Dictionary<string, string> replacedTraits = new Dictionary<string, string>
+        {
+            { "VTE_Submissive", "ST_Submissive" }
+        };
+
         public static Harmony harmony;
         static TraitUtils()
         {
             harmony = new Harmony("SimsTraitsMod");
             harmony.PatchAll();
+            if (VETraitsLoaded)
+            {
+                foreach (var kvp in replacedTraits)
+                {
+                    var defToRemove = DefDatabase<TraitDef>.GetNamedSilentFail(kvp.Key);
+                    if (defToRemove != null)
+                    {
+                        DefDatabase<TraitDef>.Remove(defToRemove);
+                    }
+                }
+            }
         }
 
         public static bool HasTrait(this Pawn pawn, TraitDef traitDef)
