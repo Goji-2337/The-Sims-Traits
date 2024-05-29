3e0055678296bb4a8793bcf3b82e4badfb2a15e2
Taranchuk <nfhfyxer@gmail.com>
2024-05-29 18:09:17 +0300

    big boned

---
+++ 1.5/Source/HarmonyPatches/BodyPartDef_GetMaxHealth_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(BodyPartDef), "GetMaxHealth")]
+    public static class BodyPartDef_GetMaxHealth_Patch
+    {
+        public static void Postfix(ref float __result, Pawn pawn)
+        {
+            if (pawn.HasTrait(ST_DefOf.VTE_BigBoned))
+            {
+                __result += 2f;
+            }
+        }
+    }
+}
---
+++ 1.5/Source/HarmonyPatches/Pawn_BodySize_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace SimsTraits
+{
+    [HarmonyPatch(typeof(Pawn), "BodySize", MethodType.Getter)]
+    public static class Pawn_BodySize_Patch
+    {
+        public static void Postfix(Pawn __instance, ref float __result)
+        {
+            if (__instance.genes != null && __instance.HasTrait(ST_DefOf.VTE_BigBoned))
+            {
+                __result *= 1.6f;
+            }
+        }
+    }
+}
