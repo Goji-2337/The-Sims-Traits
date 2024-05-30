c93f1da9121d2bc7eb23dec60be4c14316dd9ef3
Taranchuk <nfhfyxer@gmail.com>
2024-05-30 11:53:46 +0300

    fixes

--- 1.5/Source/HarmonyPatches/ThoughtWorker_ExpectationsSlave_CurrentStateInternal_Patch.cs
+++ 1.5/Source/HarmonyPatches/ThoughtWorker_ExpectationsSlave_CurrentStateInternal_Patch.cs
@@ -4,8 +4,8 @@ using Verse;
 
 namespace SimsTraits
 {
-    [HarmonyPatch(typeof(ThoughtWorker_Expectations), "CurrentStateInternal")]
-    public static class ThoughtWorker_Expectations_CurrentStateInternal_Patch
+    [HarmonyPatch(typeof(ThoughtWorker_ExpectationsSlave), "CurrentStateInternal")]
+    public static class ThoughtWorker_ExpectationsSlave_CurrentStateInternal_Patch
     {
         public static void Postfix(ref ThoughtState __result, Pawn p)
         {
