0c121345aa192d4af7f846b9c924c9b65a4a6b3c
Taranchuk <nfhfyxer@gmail.com>
2026-09-16 01:43:40 +0300

    fix

--- 1.6/Source/HarmonyPatches/Pawn_JobTracker_TryTakeOrderedJob_Patch.cs
+++ 1.6/Source/HarmonyPatches/Pawn_JobTracker_TryTakeOrderedJob_Patch.cs
@@ -7,7 +7,7 @@ namespace SimsTraits
     [HarmonyPatch(typeof(Pawn_JobTracker), "TryTakeOrderedJob")]
     public static class Pawn_JobTracker_TryTakeOrderedJob_Patch
     {
-        public static void Postfix(Pawn_DraftController __instance)
+        public static void Postfix(Pawn_JobTracker __instance)
         {
             var hediff = __instance.pawn.health.hediffSet.GetFirstHediffOfDef(ST_DefOf.ST_PsychicTranceAbsentMinded);
             if (hediff != null)
